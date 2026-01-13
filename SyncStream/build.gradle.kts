67ea9b3e91064db47864f4a66ced19758f73df47
DieGon7771 <bruttodiego6@gmail.com>
2026-01-13 22:45:43 +0100

    Update build.gradle.kts

--- SyncStream/build.gradle.kts
+++ SyncStream/build.gradle.kts
@@ -2,7 +2,7 @@ dependencies {
     implementation("com.google.android.material:material:1.13.0")
 }
 
-version = 3
+version = 4
 
 cloudstream {
     description = "Organizza homepage e sincronizza Continue Watching tra dispositivi"
@@ -11,5 +11,5 @@ cloudstream {
     tvTypes = listOf("All")
     requiresResources = true
     language = "it"
-    iconUrl = "https://raw.githubusercontent.com/iltuonome/tuorepo/main/icons/syncstream.png"
+    iconUrl = "https://raw.githubusercontent.com/DieGon7771/ItaliaInStreaming/master/SyncStream/SyncStream_icon.png"
 }
